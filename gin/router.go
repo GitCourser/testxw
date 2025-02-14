182bc6d59c8510922b1c9f2e11b7336c915c44d0
Courser <GitCourser@users.noreply.github.com>
2025-02-14 18:46:22 +0800

    Update router.go

--- gin/router.go
+++ gin/router.go
@@ -3,6 +3,7 @@ package serve
 import (
 	"fmt"
 	"io/fs"
+	"log"
 	"net/http"
 	"xuanwu/gin/cron"
 	"xuanwu/static"
@@ -40,16 +41,13 @@ func (p *ApiData) Init() {
 	// RootRoute.Use(Cors())
 	RootRoute.Use(p.CookieHandler()) //使用中间件进行全局用户认证
 
-	// 新的静态资源处理
-	frontend, _ := fs.Sub(static.Assets, "dist")
-	RootRoute.StaticFS("/", http.FS(frontend))  // 直接挂载到根路径
-	
-	// 处理前端路由
-	RootRoute.NoRoute(func(c *gin.Context) {
-		c.FileFromFS("dist/index.html", http.FS(static.Assets))
-	})
-
 	routeApi := RootRoute.Group("/api") //  api接口总路由
+	filesys, err := static.StaticFS()
+	if err != nil {
+		log.Println("加载后台文件失败,web服务停止")
+		return
+	}
+	RootRoute.StaticFS("/admin", http.FS(filesys))
 
 	routeAdmin := routeApi.Group("/user") // 用户数据接口
 	routeAdmin.GET("/info", p.HandlerUserInfo)
@@ -83,6 +81,19 @@ func (p *ApiData) Init() {
 	routeCron.GET("/dellogall", cron.HandlerDeleteAllLog) //删除日志
 	routeCron.GET("/getlog", cron.HandlerGetLog)          //获取日志
 	routeCron.GET("/downlog", cron.HandlerDownloadFile)   //获取日志
+
+	// 关键点【解决页面刷新404的问题】
+	RootRoute.NoRoute(func(c *gin.Context) {
+		//设置响应状态
+		c.Writer.WriteHeader(http.StatusOK)
+		//载入首页
+		indexHTML, _ := fs.ReadFile(filesys, "index.html")
+		c.Writer.Write(indexHTML)
+		//响应HTML类型
+		c.Writer.Header().Add("Accept", "text/html")
+		//显示刷新
+		c.Writer.Flush()
+	})
 
 	//动态注册插件路由
 	if p.AddApi != nil {
