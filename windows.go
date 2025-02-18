1ccfac79920ed1fce249b5d71de982f96860b6f3
Courser <GitCourser@users.noreply.github.com>
2025-02-18 11:53:26 +0800

    Add files via upload

--- windows.go
+++ windows.go
@@ -2,29 +2,20 @@
 
 package main
 
-import (
-	// "log"
-	"syscall"
-)
+import "syscall"
 
 func hideConsoleWindow() {
-	// log.Println("隐藏控制台流程开始...")
-	// defer log.Println("隐藏流程结束")
-	// if !*hideWindow {
-	// 	return
-	// }
-
-	// 释放控制台
 	kernel32 := syscall.NewLazyDLL("kernel32.dll")
-	freeConsole := kernel32.NewProc("FreeConsole")
-	// ret, _, err := freeConsole.Call()
-	freeConsole.Call()
 
 	// 隐藏窗口
-	// getConsoleWindow := kernel32.NewProc("GetConsoleWindow")
-	// if hwnd, _, _ := getConsoleWindow.Call(); hwnd != 0 {
-	// 	user32 := syscall.NewLazyDLL("user32.dll")
-	// 	showWindow := user32.NewProc("ShowWindow")
-	// 	showWindow.Call(hwnd, 0)
-	// }
+	getConsoleWindow := kernel32.NewProc("GetConsoleWindow")
+	if hwnd, _, _ := getConsoleWindow.Call(); hwnd != 0 {
+		user32 := syscall.NewLazyDLL("user32.dll")
+		showWindow := user32.NewProc("ShowWindow")
+		showWindow.Call(hwnd, 0)
+	}
+
+	// 释放控制台
+	freeConsole := kernel32.NewProc("FreeConsole")
+	freeConsole.Call()
 }
